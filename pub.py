055188cbd91a4f7236108bc099209380124e204c
Gabriel Glatz <143537581+glatztp@users.noreply.github.com>
2024-09-11 16:35:33 -0300

    Add files via upload

--- pub.py
+++ pub.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import json
+from datetime import datetime
 
 user = 'glatz'
 password = '1239'
@@ -9,13 +10,9 @@ topico = 'senai/teste69'
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
 
-# Create and configure MQTT client
 pub = mqtt.Client()
 pub.username_pw_set(user, password)
-
 pub.on_connect = on_connect
-
-# Connect to broker and start loop
 pub.connect(broker, 1883)
 pub.loop_start()
 
@@ -25,15 +22,13 @@ try:
         if message_text.lower() == 'sair':
             break
 
+        timestamp = datetime.now().strftime('%H:%M')  # Format timestamp to hh:mm
         message = {
             "user": user,
-            "message": message_text
+            "message": message_text,
+            "timestamp": timestamp  # Add formatted timestamp
         }
         pub.publish(topico, json.dumps(message))
-        print(f"Message published to topic {topico}: {message_text}")
-
-except KeyboardInterrupt:
-    print("Disconnecting...")
 
 finally:
     pub.loop_stop()
--- sub.py
+++ sub.py
@@ -1,5 +1,9 @@
 import paho.mqtt.client as mqtt
 import json
+from colorama import init, Style, Fore
+
+# Initialize colorama
+init()
 
 user = 'glatz'
 password = '1239'
@@ -11,22 +15,19 @@ def on_message(client, userdata, msg):
         message = json.loads(msg.payload.decode('utf-8'))  # Decode payload from bytes to string and then to JSON
         user = message.get("user")
         message_text = message.get("message")
-        print(f"{user}: {message_text}")
+        timestamp = message.get("timestamp")
+        # Print user name in bold and color using colorama
+        print(f"{Style.BRIGHT}{Fore.YELLOW}{user}{Style.RESET_ALL}: {message_text} - {timestamp}")  # Print timestamp along with user and message
     except json.JSONDecodeError:
         print("Received non-JSON message:", msg.payload.decode('utf-8'))
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
-    # Subscribe to the topic
     client.subscribe(topico)
 
-# Create and configure MQTT client
 sub = mqtt.Client()
 sub.username_pw_set(user, password)
-
 sub.on_message = on_message
 sub.on_connect = on_connect
-
-# Connect to broker and start loop
 sub.connect(broker, 1883)
 sub.loop_forever()
